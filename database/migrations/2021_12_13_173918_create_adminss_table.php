d210be243f355a6fadd681ced5b0ab05b1147ad1
Slava Mora <gitmooncat@gmail.com>
2021-12-13 19:41:44 +0200

    add and ch migration

--- database/migrations/2021_12_13_173918_create_adminss_table.php
+++ database/migrations/2021_12_13_173918_create_adminss_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminssTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('adminss', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('surname');
-            $table->string('phone_number')->unique();
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('adminss');
     }
 }
