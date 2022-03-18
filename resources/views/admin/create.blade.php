4785660568f4d84c36db5f1732b04c81bf2bdfb5
Slava Mora <gitmooncat@gmail.com>
2022-03-18 20:14:40 +0200

    fix(admin): title admin create

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title')Редактирование Администратора @endsection
+@section('title')Добавление администратора @endsection
 @section('content')
 <div class="container-fluid">
     <div class="row">
